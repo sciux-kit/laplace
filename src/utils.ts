b1c25671e93c2485fb86fdfb5ebdb2acc13407b5
Acbox <acbox0328@gmail.com>
2025-05-17 14:16:37 +0800

    release: v0.0.1-alpha.1

--- src/utils.ts
+++ src/utils.ts
@@ -3,24 +3,11 @@ import { toDisplayString } from "@vue/shared"
 import { MaybeRefOrGetter } from "@vue/reactivity"
 import { effect } from "@vue/reactivity"
 import { parse } from "./parser"
+import { Context, renderRoots } from "./renderer"
 
 // eslint-disable-next-line antfu/top-level-function
 export const noop = () => {}
 
-export function sciux(literal: TemplateStringsArray, ...values: MaybeRefOrGetter<unknown>[])/* : Node[] */ {
-  const uid = Math.round(performance.now() * 100)
-  const src = literal.reduce((acc, v, i) => {
-    return `${acc}${v}${i == literal.length - 1 ? '' : `(:$_SciuxEnv_${uid}_${i}_)`}`
-  }, '').trim()
-  const ast = parse(src)
-
-  // const o = values.reduce<Context>((acc, v, i) => {
-  //   acc[`:$_SciuxEnv_${uid}_${i}_`] = v
-  //   return acc
-  // }, {})
-
-  // return renderRoots(ast, undefined, hasContext() ? shallowReactive(Object.assign(o, toRefs(getCurrentContext()))) : o)[0]
-}
 
 export default function isPlainObject(obj: any) {
   if (typeof obj !== 'object' || obj === null)
