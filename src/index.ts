e0adad928698dfb64cef9464313138b27ce1e9b3
Acbox <acbox0328@gmail.com>
2025-05-26 15:27:39 +0800

    release: v0.0.1-alpha.6

--- src/index.ts
+++ src/index.ts
@@ -10,10 +10,13 @@ export * from './flows'
 
 export * from '@vue/reactivity'
 
-import { Component } from './component'
-import { components } from './renderer'
+import { components, flows } from './renderer'
 import letComp from './builtins/let'
+import { ifFlow, elseFlow, elseIfFlow } from './flows'
 
-components.set('let', letComp as Component<string, any>)
+components.set('let', letComp)
+flows.set('if', ifFlow)
+flows.set('else', elseFlow)
+flows.set('else-if', elseIfFlow)
 
-export { components }
+export { components, flows }
