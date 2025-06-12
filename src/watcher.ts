09f6686d88e858d980f1557201ca864bf21d8bf5
Acbox <acbox0328@gmail.com>
2025-06-12 15:11:00 +0800

    fix: obverser undefined in server side

--- src/watcher.ts
+++ src/watcher.ts
@@ -5,24 +5,6 @@ export interface MutationCallbacks {
 
 const callbacks = new WeakMap<Node, MutationCallbacks>()
 
-const observer = new MutationObserver((m) => {
-  m.forEach((mutation) => {
-    if (mutation.type === 'childList') {
-      mutation.addedNodes.forEach((node) => {
-        if (callbacks.has(node)) {
-          callbacks.get(node)!.mounted?.(node)
-        }
-      })
-
-      mutation.removedNodes.forEach((node) => {
-        if (callbacks.has(node)) {
-          callbacks.get(node)!.unmounted?.(node)
-        }
-      })
-    }
-  })
-})
-
 export function setMutationCallback(node: Node | Node[], cbs: MutationCallbacks | undefined): void {
   if (Array.isArray(node)) {
     node.forEach(n => setMutationCallback(n, cbs))
@@ -38,6 +20,24 @@ export function setMutationCallback(node: Node | Node[], cbs: MutationCallbacks 
 }
 
 export function observe(node: Node | Node[], cbs: MutationCallbacks | undefined): void {
+  const observer = new MutationObserver((m) => {
+    m.forEach((mutation) => {
+      if (mutation.type === 'childList') {
+        mutation.addedNodes.forEach((node) => {
+          if (callbacks.has(node)) {
+            callbacks.get(node)!.mounted?.(node)
+          }
+        })
+
+        mutation.removedNodes.forEach((node) => {
+          if (callbacks.has(node)) {
+            callbacks.get(node)!.unmounted?.(node)
+          }
+        })
+      }
+    })
+  })
+
   if (Array.isArray(node)) {
     node.forEach(n => observe(n, cbs))
   }
